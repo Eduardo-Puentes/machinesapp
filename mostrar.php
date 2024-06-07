faca52d563a125692f7cca6d8724cd126df5f075
Eduardo-Puentes <eduardo.puentes.dev@gmail.com>
2024-06-07 11:41:25 -0600

    m

--- mostrar.php
+++ mostrar.php
@@ -16,7 +16,7 @@ if ($resultado->num_rows > 0) {
         echo '<td> <div style="display: flex; justify-content: center; align-items: start; flex-direction: column; height: 70px;">' . $fila['off_time'] . '</div></td>';
         echo '<td> <div style="display: flex; justify-content: center; align-items: start; flex-direction: column; height: 70px;">' . $fila['ubication'] . '</div></td>';
         echo '<td>  <div style="display: flex; justify-content: center; align-items: start; flex-direction: column; height: 70px;">' . (($fila['availability'] == 0) ? '<p style="background-color: #ED3636; color: white; border-radius: 20px; padding: 5px; margin: 5px;">Ocupada</p>' : '<p style="background-color: #00B69B; color: white; border-radius: 20px; padding: 5px; margin: 5px;">Disponible</p>') . '</div></td>';
-        echo '<td> <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; height: 70px;"><a href="borrarUsuario.php?id=' . $fila['id'] . '" onclick="return confirm(\'¿Estás seguro de que desea elimar esta máquina?\');"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ED3636" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+        echo '<td> <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; height: 70px;"><a href="borrar.php?id=' . $fila['id'] . '" onclick="return confirm(\'¿Estás seguro de que desea elimar esta máquina?\');"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ED3636" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
           </svg></a></div></td>';
         echo '</tr>';
